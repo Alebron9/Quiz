95a817a17928f7025003a1843d836c6377396f17
Alebron9 <alejandrolebron123896@gmail.com>
2026-09-09 17:24:52 -0400

    Change

--- main.java
+++ main.java
@@ -2,5 +2,9 @@ class Main {
     public static void main(String[] args) {
         // prints "Hello, World!" to the console
         System.out.println("Hello, World!");
+
+        System.out.println("This is a quiz application.");
+
+        System.out.println("Please answer the following questions:");
     }
 }
